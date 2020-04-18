9f6b7d5e9dad19af83635557709db0a0706fdf64
MatthewTGM <62822374+MatthewTGM@users.noreply.github.com>
2020-04-18 17:45:04 +0200

    fixed contact

--- contactform.php
+++ contactform.php
@@ -1,15 +1,16 @@
 <?php
+$visitor_email = $_POST['email'];
+$visitor_email = $_POST['date'];
+$message = $_POST['message'];
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $messgae = $_POST['message'];
+$email_from = 'no-reply@matthewtgm.ga'
+$email_subject = "new contact form submission";
+$email_body = "Email: $name\n".
+                "Date Of Submission: $date\n"
+                    "Message: $message"
 
-    $mailTo = "matthewtgm120@gmail.com"
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from your websites contact form. this is from ".$mailFrom.".\n\n".$messgae;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend")
-}
+$to = "matthewtgm100@gmail.com";
+$headers = "From $email_from \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
